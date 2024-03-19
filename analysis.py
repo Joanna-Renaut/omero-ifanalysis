6cb753f15e7b163893347412f47f52f506f54018
Joanna-Renaut <joannarenaut163@gmail.com>
2024-03-19 10:41:16 +0000

    Updated figures code

--- analysis.py
+++ analysis.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,10 +5,10 @@ import seaborn as sns
 from scipy.stats import ttest_ind
 from itertools import combinations
 from pathlib import Path
-from ifanalysis.counts import count_cells
 from ifanalysis.normalisation import normalise
 from ifanalysis.sl_analysis import combplot_hist, intensity_plot
 from ifanalysis._helper_functions import save_fig
+
 STYLE = Path('~/matplotlib_style/Style_01.mplstyle')
 plt.style.use(STYLE)
 prop_cycle = plt.rcParams["axes.prop_cycle"]
@@ -17,14 +16,8 @@ colors = prop_cycle.by_key()["color"]
 
 
 def custom_sort(val):
-    if val == 'NT' or 'DMSO':
-        return (0, val)
-    elif val == 'iMAX only':
-        return (1, val)
-    elif val == 'Pool':
-        return (2, val)
-    else:
-        return (3, val)
+    order = {'NT': 0, 'DMSO': 1, 'iMAX only': 2, 'Pool': 3, 'RAD51': 4, 'BCL2': 5}
+    return order.get(val, 6), val
 
 
 def count_per_cond(df: pd.DataFrame) -> pd.DataFrame:
@@ -63,6 +56,8 @@ if __name__ == '__main__':
         CONTROL_GENOTYPE = 'WT'
         SELECTED_CONDITIONS = []
         ALL_CONTROLS = ['DMSO']  # enter your controls here in the order you would like them in figures
+        INTENSITY_PLOT_COLUMNS = []  # enter your column names for the different stains for an intensity plot
+        RATIO_THRESHOLD = 0.7  # set the threshold for ratios shown on plot for relative cell counts
 
         for file in repeat_path.glob('*.csv'):
             data = pd.read_csv(file, index_col=0)
@@ -113,50 +108,19 @@ if __name__ == '__main__':
 
         selected_conds = SELECTED_CONDITIONS
 
-        columns = ['intensity_mean_Casp3/7_nucleus', 'intensity_mean_p21_nucleus',
-                   'intensity_mean_yH2A.X_nucleus']
+        if INTENSITY_PLOT_COLUMNS:
+            columns = INTENSITY_PLOT_COLUMNS
 
-        intensity_plot(df, selected_conds, columns, title='Intensity Plot, WT', hue='genotype')
+            intensity_plot(df, selected_conds, columns, title='Intensity Plot, WT', hue='genotype')
 
         # Get unique genotypes
         unique_genotypes = list(final_plot_df['genotype'].unique())
-
-        # Extract unique conditions
-        unique_conditions = list(final_plot_df['condition'].unique())
-
-        # Separate control and non-control conditions based on criteria
-        non_control_conditions = [cond for cond in unique_conditions if cond not in ALL_CONTROLS]
-
-        # Combine the lists, ensuring control conditions are first
-        sorted_conditions = ALL_CONTROLS + non_control_conditions
-
-        # set colours for figures
-        if len(unique_genotypes) > len(colors):
-            # Repeat colors
-            extended_colors = colors * (len(unique_genotypes) // len(colors) + 1)
-        else:
-            extended_colors = colors
-
-        # Create color mapping with available/extended colors
-        color_mapping = {genotype: color for genotype, color in zip(unique_genotypes, extended_colors)}
 
         if CONTROL_GENOTYPE in unique_genotypes:
             unique_genotypes.remove(CONTROL_GENOTYPE)
         hue_order = [CONTROL_GENOTYPE] + unique_genotypes
 
-        # Create bar plot
-        fig, ax = plt.subplots(figsize=(10, 6))
-        sns.barplot(x='condition', y='ratio', hue='genotype', data=final_plot_df, dodge=True,
-                    hue_order=hue_order, order=sorted_conditions)
-
-        plt.axhline(y=0.9, color='gray', linestyle='--', linewidth=1)  # add line at the desired ratio level
-        plt.xticks(rotation=90)
-        plt.tight_layout()
-        ax.legend(loc='upper right')
-        save_fig(fig, repeat_path, f'screen{repeat_number}_ratios')
-        plt.savefig(f'{repeat_path}/screen{repeat_number}_ratios.png', dpi=600)
-
-        # create these figures for each genotype (cell line) in the experiment
+        # create figures for each genotype (cell line) in the experiment
         for genotype in unique_genotypes:
 
             df1 = df[df.genotype == 'WT']
@@ -170,18 +134,11 @@ if __name__ == '__main__':
             combplot_hist(df1_cc, selected_conds, 'CellCycle Histogram, WT')
             combplot_hist(df2_cc, selected_conds, f'CellCycle Histogram, {genotype}')
 
-            # Extract unique conditions
-            unique_conditions = list(final_plot_df['condition'].unique())
-
-            # Separate control and non-control conditions based on criteria
-            non_control_conditions = [cond for cond in unique_conditions if cond not in ALL_CONTROLS]
-
-            # Combine the lists, ensuring control conditions are first
-            sorted_conditions = ALL_CONTROLS + non_control_conditions
+            sorted_values = sorted(final_plot_df.condition.unique(), key=custom_sort)
 
             fig, ax = plt.subplots(figsize=(6, 6))
             sns.barplot(x='condition', y='abs_cell_count', hue='genotype', data=final_plot_df, ax=ax,
-                        hue_order=hue_order, order=sorted_conditions)
+                        hue_order=hue_order, order=sorted_values)
             plt.xticks(rotation=90)
             ax.legend(loc='upper right')
 
@@ -195,16 +152,41 @@ if __name__ == '__main__':
 
                 # Check if positions and subset lengths match before plotting
                 if not subset.empty and len(positions) == len(subset):
-
                     # Correctly calculate positions for this genotype's bars
                     positions = np.arange(len(sorted_values)) + (
-                            i - len(hue_order) / 2.0) * 0.2  # Adjust the 0.2 if necessary to align with your bar width
+                        i - len(hue_order) / 2.0) * 0.2  # Adjust the 0.2 if necessary to align with your bar width
                     # Plot error bars
                     plt.errorbar(positions, subset['abs_cell_count'], yerr=subset['abs_cell_count_sem'], fmt='none',
                                  color='black', capsize=3, elinewidth=1)
 
             save_fig(fig, repeat_path, f'screen{repeat_number}_hits_abscounts')
             plt.savefig(f'{repeat_path}/screen{repeat_number}_hits_abscounts.png', dpi=600)
+
+            # Sort by condition using custom_sort for plotting
+            sorted_conditions = sorted(final_plot_df['condition'].unique(), key=custom_sort)
+            final_plot_df['condition'] = pd.Categorical(final_plot_df['condition'], categories=sorted_conditions,
+                                                        ordered=True)
+            final_plot_df.sort_values(by=['condition', 'genotype'], inplace=True)
+
+            final_genotype = final_plot_df[final_plot_df['genotype'] == genotype].copy()
+
+            bar_colors = [colors[1] if ratio < RATIO_THRESHOLD else colors[0] for ratio in
+                          final_genotype.groupby('condition', observed=True)['ratio'].mean()]
+
+            # Create bar plot for ratios
+            fig, ax = plt.subplots(figsize=(10, 6))
+            sns.barplot(x='condition', y='ratio', data=final_genotype, ax=ax, palette=bar_colors,
+                        order=sorted_conditions)
+
+            plt.axhline(y=RATIO_THRESHOLD, color='gray', linestyle='--', linewidth=2)
+            plt.xticks(rotation=90)
+            plt.tight_layout()
+
+            # Save the figure
+            fig_filename = f'screen{repeat_number}_ratios_{genotype}'
+            save_fig(fig, repeat_path, fig_filename)
+            plt.savefig(f'{repeat_path}/{fig_filename}.png', dpi=600)
+            plt.close(fig)
 
         # Aggregate abs_cell_count by experiment, condition, and genotype to get mean values
         aggregated_df = df_counts.groupby(['experiment', 'condition', 'genotype'], as_index=False).agg({
